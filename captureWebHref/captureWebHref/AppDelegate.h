c652857398487c8d056cc9e0d1dd56b81560980c
Alex Ramos <alex.ramos@ideaknow.com>
2015-10-13 11:40:54 +0200

    implement app delegate and create navigationController

---
+++ captureWebHref/captureWebHref/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  captureWebHref
+//
+//  Created by Alejandro Ramos Grifé on 6/10/15.
+//  Copyright © 2015 visualEngineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
