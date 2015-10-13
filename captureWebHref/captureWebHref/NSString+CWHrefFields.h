f4ad50bb0dd7e5c805b1b80f478f72649ea46cb2
Alex Ramos <alex.ramos@ideaknow.com>
2015-10-13 13:22:07 +0200

    add comments like Oxygen style

--- captureWebHref/captureWebHref/NSString+CWHrefFields.h
+++ captureWebHref/captureWebHref/NSString+CWHrefFields.h
@@ -12,11 +12,15 @@
 
 /**
  * Class method to find all the list of "a" tags with regular expression
+ * @param htmlSourceCode
+ * @return NSArray with list of "a" tag fields
  */
 + (NSArray *)listOfATagFields:(NSString *)htmlSourceCode;
 
 /**
  * Class method to extract only hrefs in each "a" tag
+ * @param dirtyString
+ * @return NSString with each url of href
  */
 + (NSString *)extractHrefFromString:(NSString *)dirtyString;
 
