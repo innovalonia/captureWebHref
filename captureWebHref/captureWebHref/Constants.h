e3ba215038161ab696165f581cff8df928a5a915
Alex Ramos <alex.ramos@ideaknow.com>
2015-10-13 11:46:22 +0200

    add constants to project

---
+++ captureWebHref/captureWebHref/Constants.h
@@ -0,0 +1,11 @@
+//
+//  Constants.h
+//  captureWebHref
+//
+//  Created by Alejandro Ramos Grifé on 7/10/15.
+//  Copyright © 2015 visualEngineering. All rights reserved.
+//
+
+static NSString *const urlString = @"http://www.visual-engin.com/Web/index.php";
+static NSString *const kFinishLoading = @"finishLoading";
+
