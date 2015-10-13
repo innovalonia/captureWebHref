a740de0d21a493cdd9950ddfbedaa58e6982071c
Alex Ramos <alex.ramos@ideaknow.com>
2015-10-13 11:59:30 +0200

    add custom cell

---
+++ captureWebHref/captureWebHref/HrefCell.h
@@ -0,0 +1,13 @@
+//
+//  HrefCell.h
+//  captureWebHref
+//
+//  Created by Alejandro Ramos Grifé on 8/10/15.
+//  Copyright © 2015 visualEngineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HrefCell : UITableViewCell
+
+@end
---
+++ captureWebHref/captureWebHref/HrefCell.m
@@ -0,0 +1,16 @@
+//
+//  HrefCell.m
+//  captureWebHref
+//
+//  Created by Alejandro Ramos Grifé on 8/10/15.
+//  Copyright © 2015 visualEngineering. All rights reserved.
+//
+
+#import "HrefCell.h"
+
+/**
+ * A cell that displays information of a given href
+ */
+@implementation HrefCell
+
+@end
